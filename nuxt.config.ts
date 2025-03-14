b3edf32456a06285c610d6b078fd675a45896279
semicolon108 <sabaidee.semicolon@gmail.com>
2025-03-14 17:23:22 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-
+  ssr: true,
   css: [
     "@/assets/css/base.css",
     "@/assets/css/font.css",
