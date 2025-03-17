442fd390ae28cb6fb54292fa9e4dc9e19826e848
semicolon108 <sabaidee.semicolon@gmail.com>
2025-03-17 10:47:02 +0700

    update favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,30 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   ssr: true,
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/vipo-icon.svg" },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/vipo-icon.svg",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/vipo-icon.svg",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/vipo-icon.svg",
+        },
+      ],
+    },
+  },
   css: [
     "@/assets/css/base.css",
     "@/assets/css/font.css",
